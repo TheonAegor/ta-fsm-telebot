9b5420e4785aacd7a843066727ed554171040fa8
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-05-02 23:35:59 +0500

    chore: delete TODO

--- strategy.go
+++ strategy.go
@@ -1,6 +1,8 @@
 package fsm
 
 import (
+	"fmt"
+
 	tele "gopkg.in/telebot.v3"
 )
 
@@ -12,9 +14,29 @@ const (
 	StrategyGlobalUser
 	StrategyUserInTopic
 	StrategyChatTopic
+	_maxStrategy
 
 	StrategyDefault = StrategyUserInChat
 )
+
+// explicit set type for check in compile helps
+// check if items in array don't equal actually count of values
+//
+//goland:noinspection GoVarAndConstTypeMayBeOmitted
+var strategyStr [_maxStrategy]string = [...]string{
+	StrategyUserInChat:  "StrategyUserInChat",
+	StrategyChat:        "StrategyChat",
+	StrategyGlobalUser:  "StrategyGlobalUser",
+	StrategyUserInTopic: "StrategyUserInTopic",
+	StrategyChatTopic:   "StrategyChatTopic",
+}
+
+func (s Strategy) String() string {
+	if 0 <= s && s <= _maxStrategy {
+		return strategyStr[s]
+	}
+	return fmt.Sprintf("Strategy(%d)", s)
+}
 
 func (s Strategy) Apply(botId int64, chatId int64, userId int64, threadId int64) StorageKey {
 	switch s {
@@ -58,7 +80,7 @@ func (s Strategy) Apply(botId int64, chatId int64, userId int64, threadId int64)
 func ExtractKeyWithStrategy(c tele.Context, strategy Strategy) StorageKey {
 	chatId := c.Chat().ID
 	userId := c.Sender().ID
-	threadId := int64(c.Message().ThreadID) // TODO: check is safe using zero thread id
+	threadId := int64(c.Message().ThreadID)
 
 	bot := c.Bot().Me
 	return strategy.Apply(bot.ID, chatId, userId, threadId)
